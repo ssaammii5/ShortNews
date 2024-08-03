14034fd7d0f487ab1ab23f395397cb10f96e6761
Sami <47335566+ssaammii5@users.noreply.github.com>
2024-08-03 20:08:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/shortnews/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.shortnews
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.shortnews.ui.theme.ShortNewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            ShortNewsTheme {
+                Greeting(name = "world")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShortNewsTheme {
+        Greeting("Android")
+    }
+}
